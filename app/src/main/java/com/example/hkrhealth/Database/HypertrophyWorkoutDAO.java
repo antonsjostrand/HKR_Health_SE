6888ae1da920bd8614b3db72b461b59fde6e1117
antonsjostrand <anton.sjostrand@hotmail.se>
2019-05-31 01:22:33 +0200

    Added statistics for hyper workouts

--- app/src/main/java/com/example/hkrhealth/Database/HypertrophyWorkoutDAO.java
+++ app/src/main/java/com/example/hkrhealth/Database/HypertrophyWorkoutDAO.java
@@ -24,6 +24,15 @@ public interface HypertrophyWorkoutDAO {
     @Query("SELECT * FROM hypertrophy_workout WHERE workoutID = :workoutID")
     LiveData<HypertrophyWorkout> getHypertrophyWorkoutByID(int workoutID);
 
+    @Query("SELECT * FROM hypertrophy_workout WHERE workout_type = 'Push'")
+    LiveData<List<HypertrophyWorkout>> getAllHypertrophyPushWorkouts();
+
+    @Query("SELECT * FROM hypertrophy_workout WHERE workout_type = 'Pull'")
+    LiveData<List<HypertrophyWorkout>> getAllHypertrophyPullWorkouts();
+
+    @Query("SELECT * FROM hypertrophy_workout WHERE workout_type = 'Legs'")
+    LiveData<List<HypertrophyWorkout>> getAllHypertrophyLegsWorkouts();
+
     }
 
 
